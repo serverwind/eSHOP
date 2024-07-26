ad6f9916fc4e01da4908e4b631fd21031b84b326
lestrangeqq <lestrangeqq@gmail.com>
2024-07-26 21:27:44 +0300

    Basic markup for main area

---
+++ src/components/Shop.jsx
@@ -0,0 +1,14 @@
+import Filter from "./Filter"
+
+const Shop = () => {
+    return (
+        <main className="flex flex-col gap-4">
+            <Filter />
+            <section>
+                <h2>Offers</h2>
+            </section>
+        </main>
+    )
+}
+
+export default Shop
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import './output.css'
 import Header from './components/Header'
+import Shop from './components/Shop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
     <Header />
+    <Shop />
+    </>
 )
